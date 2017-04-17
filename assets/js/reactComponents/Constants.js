bf1ef651eb12d62f3e56fc91541f2a2664bfd895
GyeongSeok Seo <gyseok.seo@samsung.com>
2017-04-18 08:11:25 +0900

    Add ppt description contents

--- assets/js/reactComponents/Constants.js
+++ assets/js/reactComponents/Constants.js
@@ -315,6 +315,32 @@ const carrerSummaryValues ='\n \
 2009.03 ~ 2010.03 : Core3실 Framework Team - 호환 DLL 개발 - MFC\n \
 ';
 
+const pptDatas = [{
+    title: 'Virtual DOM',
+    url: 'https://www.slideshare.net/gyeongseokseo/virtual-dom',
+    image: './images/ppt/virtualDom.png',
+    desc: 'This is my virtual dom slide share ppt. Virtual DOM is a technique and set of libraries / algorithms that allow us to improve front end performance by avoiding direct work with DOM and work only with lightweight JavaScript object that mimics the DOM tree.'
+},{
+    title: 'WebAssembly',
+    url: 'https://www.slideshare.net/gyeongseokseo/web-assembly-70493156',
+    image: './images/ppt/webAssembly.png',
+    desc: 'This is my webassembly slide share ppt. WebAssembly or wasm is a new portable, size- and load-time-efficient format suitable for compilation to the web.'
+},{
+    title: 'Protractor',
+    url: 'https://www.slideshare.net/gyeongseokseo/protractor-web-ui-test',
+    image: './images/ppt/protractor.png',
+    desc: 'This is my protractor slide share ppt. Protractor is a web UI/UX auto testing framework, and end-to-end test framework for Angular and AngularJS applications. Protractor runs tests against your application running in a real browser, interacting with it as a user would.'
+},{
+    title: 'Node.js',
+    url: 'http://gseok.github.io/ppt/nodejs/',
+    image: './images/ppt/nodejs.png',
+    desc: 'This is my node.js study web-ppt. Node.js is a platform built on Chrome\'s JavaScript runtime for easily building fast, scalable network applications. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient, perfect for data-intensive real-time applications that run across distributed devices.'
+},{
+    title: 'HTML5 Canvas',
+    url: 'http://gseok.github.io/ppt/canvas/',
+    image: './images/ppt/html5canvas.png',
+    desc: 'This is my HTML5 canvas study web-ppt. The HTML5 canvas element is used to draw graphics, on the fly, via scripting (usually JavaScript). The canvas element is only a container for graphics. You must use a script to actually draw the graphics. Canvas has several methods for drawing paths, boxes, circles, characters, and adding images.'
+}];
 
 module.exports = {
     ABOUT_ME_CODE_STRING: aboutMeCodeString,
@@ -323,5 +349,6 @@ module.exports = {
     ABOUT_ME_LIKES: aboutMeLikesString,
     TIME_LINE_VALUES: timeLineValues,
     CARRER_TABLE_VALUES: carrerTableValues,
-    CARRER_SUMMARY_VALUES: carrerSummaryValues
+    CARRER_SUMMARY_VALUES: carrerSummaryValues,
+    PPT_DATA: pptDatas
 };
--- assets/js/reactComponents/about.js
+++ assets/js/reactComponents/about.js
@@ -542,49 +542,25 @@ class TechPPT extends React.Component {
         super(props);
 
         this.state = {
-            label: 'Tech PPT'
+            label: 'Tech PPT',
+            pptIndex: 0
         };
 
         this.onChange = this.onChange.bind(this);
     }
 
     onChange(pptIndex) {
+        this.setState({pptIndex: pptIndex});
     }
 
     createContents() {
-        const pptDatas = [{
-            title: 'Virtual DOM',
-            url: 'https://www.slideshare.net/gyeongseokseo/virtual-dom',
-            image: './images/ppt/virtualDom.png',
-            desc: 'This is my virtual dom slide share ppt. Virtual DOM is a technique and set of libraries / algorithms that allow us to improve front end performance by avoiding direct work with DOM and work only with lightweight JavaScript object that mimics the DOM tree.'
-        },{
-            title: 'WebAssembly',
-            url: 'https://www.slideshare.net/gyeongseokseo/web-assembly-70493156',
-            image: './images/ppt/webAssembly.png',
-            desc: 'This is my webassembly slide share ppt. WebAssembly or wasm is a new portable, size- and load-time-efficient format suitable for compilation to the web.'
-        },{
-            title: 'Protractor',
-            url: 'https://www.slideshare.net/gyeongseokseo/protractor-web-ui-test',
-            image: './images/ppt/protractor.png',
-            desc: 'This is my protractor slide share ppt. Protractor is a web UI/UX auto testing framework, and end-to-end test framework for Angular and AngularJS applications. Protractor runs tests against your application running in a real browser, interacting with it as a user would.'
-        },{
-            title: 'Node.js',
-            url: 'http://gseok.github.io/ppt/nodejs/',
-            image: './images/ppt/nodejs.png',
-            desc: 'This is my node.js study web-ppt. Node.js is a platform built on Chrome\'s JavaScript runtime for easily building fast, scalable network applications. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient, perfect for data-intensive real-time applications that run across distributed devices.'
-        },{
-            title: 'HTML5 Canvas',
-            url: 'http://gseok.github.io/ppt/canvas/',
-            image: './images/ppt/html5canvas.png',
-            desc: 'This is my HTML5 canvas study web-ppt. The HTML5 canvas element is used to draw graphics, on the fly, via scripting (usually JavaScript). The canvas element is only a container for graphics. You must use a script to actually draw the graphics. Canvas has several methods for drawing paths, boxes, circles, characters, and adding images.'
-        }];
-
-        const pptViews = pptDatas.map((entry) => {
+        const pptData = Constants.PPT_DATA;
+        const pptViews = pptData.map((entry) => {
             return (
                 <div key={entry.url}>
-                    <Flex align='center' justify="center">
+                    <Flex align='center' justify='center'>
                         <Box col={12} lg={12} md={12} sm={12}>
-                            <a href={entry.url}>
+                            <a href={entry.url} target='_blank'>
                                 <img src={entry.image}/>
                             </a>
                         </Box>
@@ -598,6 +574,11 @@ class TechPPT extends React.Component {
                 <Carousel afterChange={this.onChange}>
                     {pptViews}
                 </Carousel>
+                <div className='my-ppt-descs-warp'>
+                    <h5>{pptData[this.state.pptIndex].title}</h5>
+                    <p></p>
+                    <p>{pptData[this.state.pptIndex].desc}</p>
+                </div>
             </div>
         );
     }
