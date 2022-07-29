65832f523017e1cbcc54820a28df2d72da9600eb
GyeongSeokSeo <gyseok.seo@navercorp.com>
2022-07-29 19:14:58 +0900

    feat: v4.0 Add tech ppt carocell

--- src/components/about-sections/techppt-section/index.js
+++ src/components/about-sections/techppt-section/index.js
@@ -1,6 +1,7 @@
 import React, { useCallback, useState, useMemo, useEffect } from 'react';
-import { Card, Carousel } from 'antd';
+import { Card } from 'antd';
 import { Flex, Box } from 'reflexbox';
+import Carousel from 'nuka-carousel';
 import Constants from './pptConstants';
 import SectionHeader from '../../section-header';
 import Image from '../../image';
@@ -28,48 +29,43 @@ const TechPPTSection = () => {
   const contents = useMemo(() => {
     return pptData.map((entry) => {
       return (
-        <div key={entry.url}>
-          <a href={entry.image} className="my-ppt-item-wrap">
-            <Image
-              className="my-ppt-item"
-              data-test={new Date().getTime()}
-              src={entry.image}
-              alt={entry.title}
-              onClick={onClick(entry.url)}
-              onKeyPress={() => {}}
-              // eslint-disable-next-line jsx-a11y/no-noninteractive-element-to-interactive-role
-              role="button"
-              tabIndex={0}
-              aria-label={entry.title}
-            />
-          </a>
+        <div key={entry.url} className="my-ppt-top-wrap">
+          <Flex align="center" justify="center">
+            <Box col={12} lg={12} md={12} sm={12}>
+              <button className="my-ppt-item-wrap" onClick={onClick(entry.url)}>
+                <Image
+                  className="my-ppt-item"
+                  data-test={new Date().getTime()}
+                  src={entry.image}
+                  alt={entry.title}
+                  onKeyPress={() => {}}
+                  // eslint-disable-next-line jsx-a11y/no-noninteractive-element-to-interactive-role
+                  role="button"
+                  tabIndex={0}
+                  aria-label={entry.title}
+                />
+              </button>
+            </Box>
+          </Flex>
         </div>
       );
     });
   }, []);
 
-  console.log('.. ppt...!!@!');
-
-  const entry = pptData[pptIndex];
-
   return (
     <Card className="my-ant-card">
       <SectionHeader title="Tech PPT" />
       <div>
-          <a href={entry.image} className="my-ppt-item-wrap">
-            <Image
-              className="my-ppt-item"
-              data-test={new Date().getTime()}
-              src={entry.image}
-              alt={entry.title}
-              onClick={onClick(entry.url)}
-              onKeyPress={() => {}}
-              // eslint-disable-next-line jsx-a11y/no-noninteractive-element-to-interactive-role
-              role="button"
-              tabIndex={0}
-              aria-label={entry.title}
-            />
-          </a>
+        <Carousel
+          pauseOnHover
+          cellAlign="center"
+          afterSlide={(index) => {
+            setPPTIndex(index);
+          }}
+          beforeSlide={(_cur, end) => {
+            setPPTIndex(end);
+          }}
+        >{contents}</Carousel>
         <div className="my-ppt-descs-warp">
           <h5>{pptData[pptIndex].title}</h5>
           <p />
